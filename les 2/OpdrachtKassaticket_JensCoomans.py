b032cd98e4f5a67607168c906d989213c0112736
wackojens <wackojens2@hotmail.com>
2021-11-17 11:15:36 +0100

    initial commit

---
+++ les 2/OpdrachtKassaticket_JensCoomans.py
@@ -0,0 +1,93 @@
+import pcinput
+import datetime
+now = datetime.datetime.now()
+prijsMosselen = 20
+prijsKoninginnenhapje = 10
+prijsIJs = 3
+prijsDrank = 2
+korting = 0
+tempTotaal = 0
+
+totaalMosselen = pcinput.getInteger('Geef het aantal porties mosselen in: ')
+totaalKoninginnenhapje = pcinput.getInteger('Geef het aantal porties koninginnenhapje in: ')
+totaalIJs = pcinput.getInteger('Geef het aantal ijsjes in: ')
+totaalDrank = pcinput.getInteger('Geef het aantal drankjes in: ')
+totaal =(prijsMosselen * totaalMosselen) + (prijsKoninginnenhapje * totaalKoninginnenhapje) + (prijsIJs * totaalIJs) + (prijsDrank * totaalDrank)
+
+tempTotaal = totaal
+
+while True:
+    try:
+        ontvangen = float(input('Geef ontvangen bedrag in: '))
+        break
+    except ValueError:
+        print('verkeerde input. Geef bedrag opnieuw in')
+
+if totaal>50 and totaalMosselen>=2:
+    korting = 5
+    totaalMetKorting = totaal - korting
+    wisselgeld = ontvangen - totaalMetKorting
+    tempTotaal = totaalMetKorting
+else:
+    wisselgeld = ontvangen - totaal
+
+while True:
+    if wisselgeld<0:
+        print('Te weinig betaald. Controleer ontvagen bedrag')
+        ontvangen = float(input('Geef ontvangen bedrag in: '))
+        wisselgeld = ontvangen - tempTotaal
+    else:
+        break
+
+totaal500, rest = pcinput.berekenMuntstukken(wisselgeld, 500)
+totaal200, rest = pcinput.berekenMuntstukken(rest, 200)
+totaal100, rest = pcinput.berekenMuntstukken(rest, 100)
+totaal50, rest = pcinput.berekenMuntstukken(rest, 50)
+totaal20, rest = pcinput.berekenMuntstukken(rest, 20)
+totaal10, rest = pcinput.berekenMuntstukken(rest, 10)
+totaal5, rest = pcinput.berekenMuntstukken(rest, 5)
+totaal2, rest = pcinput.berekenMuntstukken(rest, 2)
+totaal1, rest = pcinput.berekenMuntstukken(rest, 1)
+totaal50C, rest = pcinput.berekenMuntstukken(rest, 0.50)
+totaal20C, rest = pcinput.berekenMuntstukken(rest, 0.20)
+totaal10C, rest = pcinput.berekenMuntstukken(rest, 0.10)
+totaal5C, rest = pcinput.berekenMuntstukken(rest, 0.05)
+totaal2C, rest = pcinput.berekenMuntstukken(rest, 0.02)
+totaal1C, rest = pcinput.berekenMuntstukken(rest, 0.01)
+
+print(f"{'Kassaticket':^60}")
+print('*'*60)
+print(f"{'U werd geholpen door:':<30}{'Jens':>30}")
+print(f"{now.strftime('%Y-%m-%d'):<30}{now.strftime('%H:%M:%S'):>30}")
+print('*'*60)
+print(f"{'Gerecht':<28}{'Aantal':<16}{'Bedrag':>16}")
+print(f"{'Mosselen':<30}{totaalMosselen:<15}{totaalMosselen * prijsMosselen:>15.2f}")
+print(f"{'Koninginnenhapje':<30}{totaalKoninginnenhapje:<15}{totaalKoninginnenhapje * prijsKoninginnenhapje:>15.2f}")
+print(f"{'IJs':<30}{totaalIJs:<15}{totaalIJs * prijsIJs:>15.2f}")
+print(f"{'Drank':<30}{totaalDrank:<15}{totaalDrank * prijsDrank:>15.2f}")
+print('='*60)
+print(f"{'Totaal te betalen:':<50}{totaal:>10.2f}")
+if korting>0:
+    print(f"{'Korting:':<50}{5:>10.2f}")
+    print(f"{'Totaal met korting:':<50}{totaalMetKorting:>10.2f}")
+print('-'*60)
+print(f"{'Betaald:':<50}{ontvangen:>10.2f}")
+print(f"{'Wisselgeld:':<50}{wisselgeld:>10.2f}")
+pcinput.muntenWisselgeld(totaal500, 500)
+pcinput.muntenWisselgeld(totaal200, 200)
+pcinput.muntenWisselgeld(totaal100, 100)
+pcinput.muntenWisselgeld(totaal50, 50)
+pcinput.muntenWisselgeld(totaal20, 20)
+pcinput.muntenWisselgeld(totaal10, 10)
+pcinput.muntenWisselgeld(totaal5, 5)
+pcinput.muntenWisselgeld(totaal2, 2)
+pcinput.muntenWisselgeld(totaal1, 1)
+pcinput.muntenWisselgeld(totaal50C, 0.50)
+pcinput.muntenWisselgeld(totaal20C, 0.20)
+pcinput.muntenWisselgeld(totaal10C, 0.10)
+pcinput.muntenWisselgeld(totaal5C, 0.05)
+pcinput.muntenWisselgeld(totaal2C, 0.02)
+pcinput.muntenWisselgeld(totaal1C, 0.01)
+print('*'*60)
+print(f"{'Geniet van uw maaltijd en nog een prettige dag!':^60}")
+print('*'*60)
---
+++ les 2/pcinput.py
@@ -0,0 +1,58 @@
+def getFloat( prompt ):
+    while True:
+        try:
+            num = float( input( prompt ) )
+        except ValueError:
+            print( "That is not a number -- please try again" )
+            continue
+        return num
+
+def getInteger( prompt ):
+    while True:
+        try:
+            num = int( input( prompt ) )
+        except ValueError:
+            print( "That is not an integer -- please try again" )
+            continue
+        return num
+
+def getString( prompt ):
+    line = input( prompt )
+    return line.strip()
+
+def getLetter( prompt ):
+    while True:
+        line = input( prompt )
+        line = line.strip()
+        line = line.upper()
+        if len( line ) != 1:
+            print( "Please enter exactly one character" )
+            continue
+        if line < 'A' or line > 'Z':
+            print( "Please enter a letter from the alphabet" )
+            continue
+        return line
+
+def vraagInput(vraag):
+    totaal = 0
+    while True:
+        try:
+            totaal =int(input(vraag))
+            break
+        except ValueError:
+            print('verkeerde input. Geef aantal opnieuw in')
+    return totaal
+
+def berekenMuntstukken(bedrag, muntstuk):
+    aantal = 0
+    aantal = bedrag // muntstuk
+    bedrag = bedrag % muntstuk
+    return int(aantal), round(bedrag, 2)
+
+def muntenWisselgeld(aantal, muntstuk):
+    if aantal >0:
+        print(aantal, end=' ')
+        if muntstuk>=5:
+            print('Briefje(s) van', muntstuk, 'EUR')
+        else:
+            print('munt(en) van', muntstuk, 'EUR')
