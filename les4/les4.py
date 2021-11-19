efe64fac322a8b80cedf94723f1ad20eeda3f7cd
wackojens <wackojens2@hotmail.com>
2021-11-19 09:29:21 +0100

    test

--- les4/les4.py
+++ les4/les4.py
@@ -1,10 +1,5 @@
-from pcinput import getInteger
-totaal = 0
-num = getInteger('Geef een nummer (0 om te stoppen): ')
 teller = 0
-while num != 0:
-    totaal += num
-    num = getInteger('Geef een nummer (0 om te stoppen): ')
+
+while teller < 5:
+    print('Python is leuk!')
     teller += 1
-print('Totaal is', totaal)
-print('Gemiddelde is', round((totaal/teller), 2) )
