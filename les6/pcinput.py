9da8312d950821ae2c20b0c35a1c6a3badad2e69
wackojens <wackojens2@hotmail.com>
2021-11-26 15:26:32 +0100

    d

--- les6/pcinput.py
+++ les6/pcinput.py
@@ -32,37 +32,3 @@ def getLetter( prompt ):
             print( "Please enter a letter from the alphabet" )
             continue
         return line
-
-def vraagInput(vraag):
-    totaal = 0
-    while True:
-        try:
-            totaal =int(input(vraag))
-            break
-        except ValueError:
-            print('verkeerde input. Geef aantal opnieuw in')
-    return totaal
-
-def berekenMuntstukken(bedrag, muntstuk):
-    aantal = 0
-    aantal = bedrag // muntstuk
-    bedrag = bedrag % muntstuk
-    return int(aantal), round(bedrag, 2)
-
-def muntenWisselgeld(aantal, muntstuk):
-    if aantal >0:
-        print(aantal, end=' ')
-        if muntstuk>=5:
-            print('Briefje(s) van', ' ', muntstuk, 'EUR', sep='')
-        else:
-            print('munt(en) van', ' ', muntstuk, 'EUR', sep='')
-
-def kortingCalculate(aantalMosselen, kostprijs):
-    discount = 0
-    if aantalMosselen>=2 and kostprijs>=150:
-        discount = 20
-    elif aantalMosselen>=2 and kostprijs<150 and kostprijs>=100:
-        discount = 10
-    elif aantalMosselen>=2 and kostprijs<100 and kostprijs>=50:
-        discount = 5
-    return discount
