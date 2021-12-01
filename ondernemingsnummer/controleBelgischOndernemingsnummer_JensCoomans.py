5686fc5aea3ded81b5cd1e60c6659b37e5f71d1f
wackojens <wackojens2@hotmail.com>
2021-12-01 22:14:19 +0100

    ondernemingsnummer methode 2 toegevoegd

--- ondernemingsnummer/controleBelgischOndernemingsnummer_JensCoomans.py
+++ ondernemingsnummer/controleBelgischOndernemingsnummer_JensCoomans.py
@@ -1,6 +1,7 @@
 import inputValidation
 
-ondernemingsnummer = inputValidation.getValidInput()
+########## X en Y enkel nodig voor tweede methode ########
+ondernemingsnummer, x, y = inputValidation.getValidInput()
 
 controleNummer = ondernemingsnummer % 100
 getal = int((ondernemingsnummer - controleNummer) / 100)
@@ -10,3 +11,19 @@ if controleNummer != check:
     print('Ongeldig nummer')
 else:
     print('Geldig nummer')
+
+# Methode 2 
+# Aangezien deze oefening over de les 'strings' gaat, is de volgende methode mss meer van toepassing.
+####################################################################################################
+import inputValidation
+
+####### X enkel nodig voor eerste methode #########
+x, getal, controleNummer = inputValidation.getValidInput()
+
+check = 97 - (int(getal) % 97)
+
+if int(controleNummer) == check:
+    print('Geldig nummer')
+else:
+    print('Ongeldig nummer')
+####################################################################################################
--- ondernemingsnummer/inputValidation.py
+++ ondernemingsnummer/inputValidation.py
@@ -16,11 +16,22 @@ def getValidInput():
         if len(number) != 9:
             print('Te weinig of te veel cijfers ingegeven.')
             continue
+        
+        # Enkel nodig voor methode 2 (om meer met de strings te werken en de les beter toe te passen.)
+        #############################################################################################
+        number1 = number[0:7]
+        number2 = number[7:]
+        #############################################################################################
 
         try:
             number = int(number)
+        # Methode 2
+        ###########################
+            number1 = int(number1)
+            number2 = int(number2)
+        ###########################
         except ValueError:
-            print('Het nummer is niet correct ingegeven. Er mogen geen letters tussen de cijfers staan.')
+            print('Het nummer is niet correct ingegeven. Er mogen geen letters of speciale tekens tussen de cijfers staan.')
             continue
-
-        return number
+        ############### number1 en number2 enkel nodig voor tweede methode
+        return number, number1, number2
