93b9f797a901241915ad495cb6e88d9c8894cee9
wackojens <wackojens2@hotmail.com>
2022-02-08 16:24:13 +0100

    venv

---
+++ how-long/tests/test_how_long.py
@@ -0,0 +1,13 @@
+from how_long import __version__
+
+
+def test_version():
+    assert __version__ == "0.1.0"
+
+
+def testFunction():
+    assert 5 == 5
+
+
+def testing():
+    assert "passed" > "failed"
